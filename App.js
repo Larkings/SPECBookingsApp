ea52194badce02bf202df51ff0c31aaa636e2422
Adrian Overdijk <adrian.overdijk@hva.nl>
2022-12-18 14:04:25 +0100

    default app orientation

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, TouchableWithoutFeedback, TouchableOpacity, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button 
+      color="blue"
+      title="Click me" 
+      onPress={() => Alert.alert("My title", "My message", [
+        {text: "Yes", onPress: () => console.log("Yes")},
+        {text: "no", onPress: () => console.log("No")}
+      ])}/>
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "orange"}
 
 const styles = StyleSheet.create({
   container: {
