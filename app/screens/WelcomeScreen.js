f58d4e27880e3222a655918b9b1fbc091db3e00e
Adrian Overdijk <adrian.overdijk@hva.nl>
2023-01-08 16:51:27 +0100

    Welcome screen basics

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,47 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import { ImageBackground, StyleSheet, View, Image, Text, TextComponent } from "react-native";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/sep09.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/spec.png")}/>
+        <Text style={{ color: "white" }}>Tagline vul het maar in!</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1
-  }
+    flex: 1,
+    justifyContent: "flex-end",
+  },  
+    loginButton: {
+    widht: "100%",
+    height: 70,
+    backgroundColor: "#cf597e",
+  },
+  logo: {
+    width: 428,
+    height: 106,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",   
+  },
+  registerButton: {
+    widht: "100%",
+    height: 70,
+    backgroundColor: "#bf194e",
+  },
+
 });
 
 export default WelcomeScreen;
