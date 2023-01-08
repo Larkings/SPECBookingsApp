2d7e63d7033d6e11822924fb9b96a27c9da1b1a0
Adrian Overdijk <adrian.overdijk@hva.nl>
2023-01-08 11:36:31 +0100

    setup welcome screen

--- App.js
+++ App.js
@@ -1,43 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  Image, 
-  Alert, 
-  TouchableWithoutFeedback, 
-  TouchableOpacity, 
-  SafeAreaView, 
-  Button } from 'react-native';
-  import {
-    useDimensions,
-    useDeviceOrientation
-  } from '@react-native-community/hooks'
+import React from "react"
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
 
 export default function App() {
-  console.log(useDimensions(), useDeviceOrientation());
-  const orientation = useDeviceOrientation();
-
-  return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button 
-      color="blue"
-      title="Click me" 
-      onPress={() => Alert.alert("My title", "My message", [
-        {text: "Yes", onPress: () => console.log("Yes")},
-        {text: "no", onPress: () => console.log("No")}
-      ])}/>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const containerStyle = { backgroundColor: "orange"}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
