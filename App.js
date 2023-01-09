59180de476e6746602a8ef2fffdddc81e4b41927
Adrian Overdijk <adrian.overdijk@hva.nl>
2023-01-09 21:04:12 +0100

    Intial navigation

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 import AppText from "./app/components/AppText/AppText";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Welcome"
+          component={WelcomeScreen}
+        />
+        <Stack.Screen name="Viewer" component={ViewImageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View, Image, Text, TextComponent } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Button,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function WelcomeScreen(props) {
+function WelcomeScreen({ navigation }) {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/sep09.jpg")}
     >
       <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/spec.png")}/>
+        <Image style={styles.logo} source={require("../assets/spec.png")} />
         <Text style={{ color: "white" }}>Tagline vul het maar in!</Text>
+        <Button title="TEST KNOP" onPress={() => navigation.push("Viewer")} />
       </View>
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
@@ -21,8 +31,8 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
-  },  
-    loginButton: {
+  },
+  loginButton: {
     widht: "100%",
     height: 70,
     backgroundColor: "#cf597e",
@@ -34,14 +44,13 @@ const styles = StyleSheet.create({
   logoContainer: {
     position: "absolute",
     top: 70,
-    alignItems: "center",   
+    alignItems: "center",
   },
   registerButton: {
     widht: "100%",
     height: 70,
     backgroundColor: "#bf194e",
   },
-
 });
 
 export default WelcomeScreen;
