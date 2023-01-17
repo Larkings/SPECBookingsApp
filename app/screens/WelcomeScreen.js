e724b53b08528439507d5bacb420b1ec4c2a5369
lloyd Larbi <lloyd.larbi@hva.nl>
2023-01-17 21:02:18 +0100

    Welcomescreen aangepast

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ImageBackground,
   StyleSheet,
@@ -6,55 +6,150 @@ import {
   Image,
   Text,
   Button,
+  TextInput,
 } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 function WelcomeScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/sep09.jpg")}
-    >
+      >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/spec.png")} />
-        <Text style={{ color: "white" }}>Tagline vul het maar in!</Text>
-        <Button title="TEST KNOP" onPress={() => navigation.push("Viewer")} />
-        <Button
-          title="ARTIST SCREEN"
-          onPress={() => navigation.push("Artists")}
-        />
+        <Button title="ARTIST SCREEN" onPress={() => navigation.push("Artists")}/>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <TextInput
+      clearButtonMode
+      onChangeText={(text) => setUsername(text)}
+      placeholder=" Username"
+      style={styles.usernameBar}
+      />
+      
+      <TextInput
+      clearButtonMode
+      secureTextEntry
+      onChangeText={(text) => setPassword(text)}
+      placeholder=" Password"
+      style={styles.passwordBar}
+      />
+
+      <View style={styles.container}>
+        <View style={styles.helpButton}>
+          <Button title=" ?  " onPress={() => alert(" shows alert for now instead of pop up")} />
+        </View>
+        <View style={styles.userNameStyle}>
+
+        </View>
+        <View style={styles.passWordStyle}>
+
+        </View>
+        <View style={styles.loginButton}>
+          <Button title="Sign In" onPress={() => navigation.push("Viewer")} />
+        </View>
+        <View style={styles.forgotPassButton}>
+          <Button title="Wachtwoord vergeten?" onPress={() => alert("You are navigated to the password recovery page")} />
+        </View>
+      </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  container:{
+    position: "absolute",
+    bottom: 60,
+    alignItems: "center",
+  },
+
   background: {
     flex: 1,
     justifyContent: "flex-end",
   },
-  loginButton: {
-    widht: "100%",
-    height: 70,
-    backgroundColor: "#cf597e",
-  },
+
   logo: {
-    width: 428,
-    height: 106,
+    width: 420,
+    height: 105,
   },
+
   logoContainer: {
     position: "absolute",
-    top: 70,
+    top: 150,
+    left:80,
     alignItems: "center",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
-  registerButton: {
-    widht: "100%",
-    height: 70,
-    backgroundColor: "#bf194e",
+
+  helpButton:{
+    widht: 40,
+    height: 40,
+    left: 250,
+    bottom: 580,
+    backgroundColor: "lightgrey",
+    borderRadius:  300,
+    justifyContent:'center',
+  },
+
+  usernameBar: {
+    height: 60,
+    fontSize: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+    margin: 20,
+    borderBottomColor: "#fff",
+    backgroundColor: "lightgrey",
+  },
+
+  passwordBar: {
+    height: 60,
+    fontSize: 20,
+    borderRadius: 10,
+    marginBottom: 320,
+    margin: 20,
+    borderBottomColor: "#fff",
+    backgroundColor: "lightgrey",
+  },
+
+  userNameStyle:{
+    borderWidth: 1,
+    borderColor:"black",
+    backgroundColor: "lightgrey",
+  },
+
+  passWordStyle:{
+    borderWidth: 1,
+    borderColor:"black",
+    backgroundColor: "lightgrey",
+  },
+
+  loginButton: {
+    left: 100,
+    bottom: 50,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    height: 60,
+    backgroundColor: "lightgrey",
+    borderRadius:  20,
+  },
+  
+  forgotPassButton: {
+    bottom: 20,
+    left: 100,
+  },
+  title: {
+    fontSize: 40,
+    color: "#fff",
+    marginBottom: 20,
+    fontWeight: "bold",
   },
 });
 
