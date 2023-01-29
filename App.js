d4c032d8d3b434b7e3c354308168227c81126249
T.R. Geldorp <tyrone.geldorp@hva.nl>
2023-01-29 11:47:36 +0100

    Made Sidebar Branch

--- App.js
+++ App.js
@@ -11,12 +11,27 @@ import SigneeScreen from "./app/screens/SigneeScreen";
 
 import AppText from "./app/components/AppText/AppText";
 
+
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+
+function Root() {
+  return (
+    <Drawer.Navigator initialRouteName='Navigatiebar'>
+      <Drawer.Screen name="WelcomeScreen" component={WelcomeScreen} />
+      <Drawer.Screen name="ArtistScreen" component={ArtistScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
+
+{/* Dit is voor navigatie */}
       <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen options={{
+        }} name="Root" component={Root} /> 
         <Stack.Screen
           options={{ headerShown: false }}
           name="Welcome"
