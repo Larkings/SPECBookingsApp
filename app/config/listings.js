c4fb18150419498bc77e06a2c092c3fa55708c04
Adrian Overdijk <adrian.overdijk@hva.nl>
2023-01-29 19:32:56 +0100

    make listing separate component

--- app/config/listings.js
+++ app/config/listings.js
@@ -4,47 +4,71 @@ export default [
     title: "Ali B",
     price: -10,
     image: require("../assets/ali-b.jpg"),
+    bio: "Toen Ali B tien jaar geleden zijn tweede en vorige album Petje Af uitbracht,\
+    bleek dat het begin van een groot avontuur. Hij werd echtgenoot en vader, maakte hij \
+    zijn debuut als televisieproducer met Ali B Op Volle Toeren, waarmee hij de Zilveren \
+    Televizierster won en begon aan een zoektocht naar muzikale diepgang en inspiratie.\
+    Die vond hij dankzij Pat Pattison, een grootmeester in songwriting die hem de fijne \
+    kneepjes van het vak bijbracht. Ali B: Ik ontdekte de waarde van intonatie en perspectief, \
+    waarmee ik mijn muziek naar het volgende niveau kon tillen. Daar is Terwijl Jullie Nog \
+    Bij Me Zijn uit voortgekomen en dat was voor mij een heel belangrijk moment. \
+    Ik kreeg weer enorm veel zin om muziek te maken en wist dat een nieuw album slechts een \
+    kwestie van tijd zou zijn. Dat album kwam er ook: Een Klein Beetje Geluk. \
+    Met singles als Ik Huil Alleen Bij Jou, Een Klein Beetje Geluk, Douane, Dat Is Money \
+    en Terwijl Jullie Nog Bij Me Zijn verkocht de The Voice of Holland-coach en SPEC.-baas \
+    platina en vergaarde hij miljoenen streams op Spotify en YouTube. Daarnaast scoorde hij \
+    voor de tiende keer een Top 10-hit in de Nederlandse Top 40 met Voy a Bailar \
+    (feat. Boef & Rolf Sanchez). Het is kristalhelder; Ali B heeft nog meer dan genoeg in \
+    zich om persoonlijk te groeien en een van de rapscene’s kopmannen te blijven in de \
+    vele jaren die hem nog resten.",
   },
   {
     id: 2,
     title: "Jordan Wayne",
     price: 1000,
     image: require("../assets/jordan_wayne_SPEC.jpg"),
+    bio: "",
   },
   {
     id: 3,
     title: "Numidia",
     price: 1000,
     image: require("../assets/numidia.jpg"),
+    bio: "",
   },
   {
     id: 4,
     title: "Ronnie Flex",
     price: 1000,
     image: require("../assets/Persfoto-01-Ronnie-Flex-min-scaled.jpg"),
+    bio: "",
   },
   {
     id: 5,
     title: "Boef",
     price: 1000,
     image: require("../assets/PERSFOTO-BOEF-02-scaled-e1588502734578.jpg"),
+    bio: "",
   },
   {
     id: 6,
     title: "Glen Faria",
     price: 1000,
     image: require("../assets/PERSFOTO-GLEN-FARIA-02-scaled.jpg"),
+    bio: "",
   },
   {
     id: 7,
     title: "Ice",
     price: 1000,
     image: require("../assets/PERSFOTO-ICE-01-min-scaled.jpg"),
+    bio: "",
   },
   {
     id: 8,
     title: "Lil Turkey Boy",
     price: 250000,
     image: require("../assets/1c8b2475-d5fa-471d-8c8f-82f8ada24174.jpg"),
+    bio: "",
   },
 ];
