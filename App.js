3f8fd2519cfdea53203253bdfad9e379a652d2d2
T.R. Geldorp <tyrone.geldorp@hva.nl>
2023-01-31 17:00:52 +0100

    Change structure in App.JS + Drawer works

--- App.js
+++ App.js
@@ -13,40 +13,36 @@ import SigneeScreen from "./app/screens/SigneeScreen";
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Welcome"
-          component={WelcomeScreen}
-        />
-        <Stack.Screen name="Viewer" component={ViewImageScreen} />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Artists"
-          component={ArtistScreen}
-        />
-        <Stack.Screen
-          //options={{ headerShown: false }}
-          name="ArtistDetails"
-          component={ArtistProfileScreen}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Booking"
-          component={BookingScreen}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Signees"
-          component={SigneeScreen}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Welcome">
+        <Drawer.Screen name="Home" component={StackNavigator} />
+        <Drawer.Screen name="Booking" component={BookingScreen} />
+        <Drawer.Screen name="Signees" component={SigneeScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
-    
   );
 }
 
-export default App;
+function StackNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Welcome"
+        component={WelcomeScreen}
+      />
+      <Stack.Screen name="Viewer" component={ViewImageScreen} />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Artists"
+        component={ArtistScreen}
+      />
+      <Stack.Screen
+        name="ArtistDetails"
+        component={ArtistProfileScreen}
+      />
+    </Stack.Navigator>
+  );
+}
